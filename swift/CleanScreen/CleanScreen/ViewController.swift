13f3210f2e074cace41b0695c33222472b5873e4
cbugnon <christophe.bugnon@musicworldmedia.com>
2020-03-28 12:05:23 +0100

    [Clear Screen] Create Clear Screen project

---
+++ swift/CleanScreen/CleanScreen/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CleanScreen
+//
+//  Created by Christophe Bugnon on 3/28/20.
+//  Copyright © 2020 Christophe Bugnon. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
